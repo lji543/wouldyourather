0d31b76355f706e499089a56f115d423848ae2c6
Illig, Laura EX1 <lillig@us.imshealth.com>
2018-12-06 09:21:01 -0800

    moving fb project

--- src/fire.js
+++ src/fire.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 
 const config = {
-  apiKey: "AIzaSyAGxHzn7GRUjigQ-b4eeCbkWp1Yq1alg-k",
-  authDomain: "wouldyourather-6362c.firebaseapp.com",
-  databaseURL: "https://wouldyourather-6362c.firebaseio.com",
-  projectId: "wouldyourather-6362c",
-  storageBucket: "wouldyourather-6362c.appspot.com",
-  messagingSenderId: "1002586172129"
+  apiKey: "AIzaSyDAHEPep2Phc7OVdtsFqUwxGrvgpOCF6qI",
+  authDomain: "wouldyourather-ff.firebaseapp.com",
+  databaseURL: "https://wouldyourather-ff.firebaseio.com",
+  projectId: "wouldyourather-ff",
+  storageBucket: "wouldyourather-ff.appspot.com",
+  messagingSenderId: "853209847755"
 };
 const fire = firebase.initializeApp(config);
 export default fire;
